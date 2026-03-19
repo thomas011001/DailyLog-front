f47e26dc0857e828f545f4f19aee1f137243a00f
thomas011001 <thomasyacoub321@gmail.com>
2026-03-19 19:15:08 +0200

    edit

--- src/context/DaysContext.jsx
+++ src/context/DaysContext.jsx
@@ -5,7 +5,7 @@ import { useDay } from "../hooks/useDay";
 
 const DaysContext = createContext(null);
 
-const useDays = () => {
+export const useDays = () => {
   const ctx = useContext(DaysContext);
   if (!ctx) throw new Error("useDays must be used within <DaysProvider>");
   return ctx;
